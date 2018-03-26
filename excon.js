3895ab254288f0b399fe243e5d68dac191668bf6
Brandon Mase <brandonofsid@gmail.com>
2018-03-25 22:04:06 -0700

    redid whole library. objects are now groupCollapsed

--- excon.js
+++ excon.js
@@ -1,5 +1,5 @@
-class Excon {
-    constructor(wantsStack, stackSize) {
+class Excon{
+    constructor(wantsStack){
         this.userAgent = '';
         this.wantsStack = wantsStack;
         this.usage = {
@@ -10,289 +10,206 @@ class Excon {
             orange:{count:0,visible:true},
             grey:{count:0,visible:true},
             green:{count:0,visible:true},
-            navy:{count:{id:2,as:3},visible:true},
+            navy:{count:0,visible:true},
             fuchsia:{count:0,visible:true},
             aqua:{count:0,visible:true},
             teal:{count:0,visible:true},
+            groupCollapsed:{count:0,visible:true},
         }
 
-        this.queue = []
+        this.queue = [];
         this.prevQueue = [];
 
-        this.fontSize = '16px';
-        this.padding = '20px 5px';
+        this.fontSize = '14px';
+        this.padding = '10px 5px';
         this.fontColor = 'white';
         this.display = 'block'
-        this.setStyle = `font-size:${this.fontSize};padding:${this.padding};padding-right:100px;color:${this.fontColor};display:block;`;
-        if (wantsStack) {
-            stackSize = typeof stackSize === 'undefined' ? 3 : stackSize;
-            navigator.userAgent.includes("Chrome") ? this.userAgent = "Chrome" : '';
-            navigator.userAgent.includes("Firefox") ? this.userAgent = "Firefox" : '';
-        }
+        this.setStyle = `font-size:${this.fontSize};padding:${this.padding};color:${this.fontColor};display:inline-block;`;
         
-        this.warn = console.warn.bind({})
-        this.log = console.log.bind({})
-        this.error = console.error.bind({})
-        
-        console.warn = (message) => {
-            this.queue.push({message:message,type:"warn"})
-            this.displayQueue();
-        }
-        
-        console.log = (message) => {
-            this.queue.push({message:message,type:"log"})
-            
-            this.displayQueue();
-        }
-        
-        console.error = (message) => {
-            this.queue.push({message:message,type:"error"})
-            this.displayQueue();
-        }
-        
-
-        // this.displayQueue()
+        navigator.userAgent.includes("Chrome") ? this.userAgent = "Chrome" : '';
+        navigator.userAgent.includes("Firefox") ? this.userAgent = "Firefox" : '';
     
-
     }
 
     displayQueue(){
-        this.log(this.queue)
-        console.clear();
         for(let i = 0;i<this.queue.length;i++){
-            // this.error(this.queue[i].type)
-            if(this.queue[i].type === "warn" && !this.queue[i].rendered){
-     
-                this.warn(this.queue[i].message)
-                
-                continue;
-            }
-            
-            if(this.queue[i].type === "error" && !this.queue[i].rendered){
+            let style = this.setStyle + `background-color:${this.queue[i].style}`
 
-                this.error(this.queue[i].message);
-                
+            if(this.queue[i].type === "groupEnd"){
+                console.groupEnd();
                 continue;
             }
-            
-            if(this.queue[i].type === "log" && !this.queue[i].rendered){
-                // this.log(this.queue[i].type)
-                this.log(this.queue[i].message)
-               
+            if(this.queue[i].type === "groupCollapsed"){
+                console.groupCollapsed(`%c${this.queue[i].message}`,style)
                 continue;
             }
 
-            if(!this.queue[i].rendered){
-           
-                if(this.usage[this.queue[i].type].visible){
-                this.log(this.queue[i].message,this.queue[i].style)
+            if(this.queue[i].type === "table"){
+                console.table(this.queue[i].message);
+                console.groupEnd();
+            }
+
+            if(!this.queue[i].type){
+                if(this.queue[i].multi){
+                   
+                    let style2 = style + ';color:black;margin-top:-100px;font-weight:bold;text-stroke:solid white 1px;'
+                    console.log(`%c${this.queue[i].message}`,style,style2)
                 }
-                
+                else{
+            console.log(`%c${this.queue[i].message}`,style)
+                }
+            this.usage[this.queue[i].style].count += 1
             }
+
+            // this.queue.splice(i,1)
         }
 
-        // this.log(this.queue)
+        this.queue = [];
 
+        window.exconUsage = this.usage;
+        window.exconQueue = this.prevQueue;
     }
 
+    defineMessage(message,style){
 
-    defineMessage(message) {
+        let isStrings = true;
+        message.map(e=>{typeof e !== "string" ? isStrings = false : ''})
+        //if everything in the message is a string just concat 
+        //the whole message and push it to the queue
+        if(isStrings){
+            this.queue.push({message:message.join(","),style:style});
+            this.prevQueue.push({message:message.join(','),style:style})
 
-        let parsedMessage = []
-
-        for(let i = 0;i<message.length;i++){
-
-            if (typeof message[i] === "object") {
-                parsedMessage.push(`${i}:` + this.objector(message[i]) + "\n");
-                continue;
-            }
-            if (Array.isArray(message[i])) {
-                parsedMessage.push(`${i}:` + this.arraytor(message[i]) + "\n")
-                continue;
-            }
-
-            parsedMessage.push(`${i}:` + message[i] + "\n");
-
-
-        }
-        let browser = this.getBrowserMessage()
-        return `${parsedMessage.join("")}\n\n${browser}`
-    }
-
-     getBrowserMessage() {
-        if(!this.wantsStack){return ''}
-        let err = new Error;
-        err = err.stack.split("\n");
-        if(this.userAgent === "Chrome"){
-            return err[4];
+            this.displayQueue();
+            return;
         }
         else{
-            return err[4]
+            this.objector(message,style);
         }
-    }
-    
-    red(){
-        let message = this.defineMessage(arguments)
 
-
-        
-        let style = this.setStyle + 'background-color:red;'
-        
-        this.queue.push({message:`%c${message}`,style:style,type:"red"})
-        this.displayQueue();
-    
+            this.displayQueue();
+            return;
     }
 
-    queue(){
-        this.log("hi")
+    parser(message){
+        let arr = [];
+            let arrCount = 0;
+            for(let key in message){
+                if(typeof message[key] === "object"){
+                let val = this.parser(message[key]);
+                arr.push({key:key,val:val});
+
+                }
+                else{
+                    let val = message[key];
+                arr.push({key:key,val:val});
+
+                }
+                
+            }
+
+            return arr
+
     }
 
-    blue(){
-        let message = this.defineMessage(arguments)
+    rebuildObj(arr,style){
+        for(let i = 0;i<arr.length;i++){
+            if(typeof arr[i].val === "object"){
+                
+                if(Array.isArray(arr[i].val) && arr[i].val.length === 0){
+                    arr[i].val = "[null]"
+                    this.queue.push({message:`${arr[i].key}: %c${arr[i].val}`,style:style,multi:true})
+                }
+                else{
+                    this.queue.push({message:arr[i].key,type:"groupCollapsed",style:style})
 
-        let style = this.setStyle + 'background-color:blue'
-        console.log(`%c${message}`,style)
-        this.usage.blue.count++
-    }
-
-    black(){
-        let message = this.defineMessage(arguments)
-
-        let style = this.setStyle + 'background-color:black'
-        console.log(`%c${message}`,style)
-        this.usage.black.count++
-    }
-
-    purple(){
-        let message = this.defineMessage(arguments)
-
-        let style = this.setStyle + 'background-color:purple'
-        console.log(`%c${message}`,style)
-        this.usage.purple.count++
-    }
-
-    orange(){
-        let message = this.defineMessage(arguments)
-
-        let style = this.setStyle + 'background-color:orange'
-        console.log(`%c${message}`,style)
-        this.usage.orange.count++
-    }
-
-    grey(){
-        let message = this.defineMessage(arguments)
-
-        let style = this.setStyle + 'background-color:grey'
-        console.log(`%c${message}`,style)
-        this.usage.grey.count++
-    }
-
-    green(){
-        let message = this.defineMessage(arguments)
-
-        let style = this.setStyle + 'background-color:green'
-        console.log(`%c${message}`,style)
-        this.usage.green.count++
-    }
-
-    navy(){
-        let message = this.defineMessage(arguments)
-
-        let style = this.setStyle + 'background-color:navy'
-        console.log(`%c${message}`,style)
-        this.usage.navy.count++
-    }
-
-    fuchsia(){
-        let message = this.defineMessage(arguments)
-
-        let style = this.setStyle + 'background-color:fuchsia'
-        console.log(`%c${message}`,style)
-        this.usage.fuchsia.count++
-    }
-
-    agua(){
-        let message = this.defineMessage(arguments)
-
-        let style = this.setStyle + 'background-color:#00FFFF;color:black;'
-        console.log(`%c${message}`,style)
-        this.usage.aqua.count++
-    }
-
-    teal(){
-        let message = this.defineMessage(arguments)
-
-        let style = this.setStyle + 'background-color:teal'
-        console.log(`%c${message}`,style)
-        this.usage.teal.count++
-    }
-
-    look(){
-        let message = this.defineMessage(arguments)
-
-        let lookStyle = 'font-size:60px;color:red;font-weight:bold;margin-bottom:-200px;'
-
-        console.log(`%cLOOK HERE`,lookStyle)
-        this.black(message)
-    }
-
-    hide(){
-        let toHide = arguments;
-        
-        for(let i = 0;i<arguments.length;i++){
-            console.log(arguments[i])
-            this.usage[arguments[i]].visible = false;
-        }
-        
-    }
-
-
-    help(){
-        let message = this.objector(this.usage);
-
-        this.black(`excon helper + usage \n ${message}`)
-    }
-
-
-
-
-    objector(message) {
-        let obj = JSON.stringify(message).split("")
-        let count = 1;
-
-        for (let i = 0; i < obj.length; i++) {
-            if (obj[i] === "{" || obj[i] === ",") {
-                obj[i] === "{" ? count++ : null
-
-                obj[i] = `${obj[i]}\n`;
-                for (let j = 0; j < count; j++) {
-                    obj[i] += "  "
+                    this.rebuildObj(arr[i].val,style)
                 }
             }
+            else{
+                
+                if(typeof arr[i].val === "string" && arr[i].val !== ''){arr[i].val = `"${arr[i].val}"`}
 
-            if (obj[i] === "}") {
-                count--;
+                if(arr[i].val === ''){
+                    arr[i].val = "%c[emptyString]"; 
+                    arr[i].multi=true
+                }
+
+                this.queue.push({message:`${arr[i].key}: ${arr[i].val}`,style:style,multi:arr[i].multi === true})
             }
         }
 
-        obj[0] = `Object:\n ${obj[0]}`
+        this.queue.push({type:"groupEnd"})
 
 
-        return obj.join("").replace(/\"/gi, "")
     }
 
-    arraytor(arr) {
-        arr = JSON.stringify(arr).split("");
-        for (let i = 0; i < arr.length; i++) {
-            (arr[i] === "]" && arr[i + 1] === ",") ? arr[i + 1] += "\n" : '';
+    parseObj(obj,style){
+        let message = obj.message
+        let count = 0;
 
+        for(let key in message){
+            if(typeof message[key] === "object"){
+                count++;
+            }
         }
-        arr[0] = `Array:\n${arr[0]}`
-        return arr.join("")
+
+        if(count === 0){
+            this.queue.push({message:obj.message,type:"table"})
+        }
+        else{
+            
+            let arr = this.parser(obj.message)
+            this.rebuildObj(arr,style);
+        }
     }
 
+    objector(message,style){
+
+        let setter = [];
+
+        //Sets all the indexs and the type of the each message
+        for(let i = 0;i<message.length;i++){
+            if(typeof message[i] === "string"){
+                setter.push({message:message[i],index:i,type:"string"});
+            }
+            else{
+                setter.push({message:message[i],index:i,type:"object"})
+            }
+        }
+
+        //If the first type is a string set the groupCollpased message to that string.
+        //If it isn't a string set the groupCollpased to just "Object"
+        if(setter[0].type === "string"){
+            this.queue.push({message:setter[0].message,style:style,type:"groupCollapsed"})
+
+            this.prevQueue.push({message:setter[0].message,style:style,type:"groupCollapsed"})
+        }
+        else{
+            this.queue.push({message:"Object",style:style,type:"groupCollapsed"})
+
+            this.prevQueue.push({message:"Object",style:style,type:"groupCollapsed"})
+            
+        }
+
+        //Get all the objects in the array and parse them.
+        let obj = setter.filter(e=>e.type === "object" ? true: false)
+
+        for(let i = 0;i<setter.length;i++){
+            if(setter[i].type === "object"){
+                this.parseObj(setter[i],style);
+            }
+        }
+
+        this.queue.push({type:"groupEnd"})
+        this.prevQueue.push({type:"groupEnd"})
+    }
+
+    red(){
+        let message = Array.from(arguments)
+        // console.log(arguments)
+        message = this.defineMessage(message,"red")
+    }
 }
 
 export let excon = new Excon(true)
-
-
